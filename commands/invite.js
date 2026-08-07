fb3bce80d7b2d7ba7e407151d24df30d883d9def
simonpodhorny13-glitch <simonpodhorny13@gmail.com>
2026-08-07 16:17:08 +0200

    Fix invite command role permission check

--- commands/invite.js
+++ commands/invite.js
@@ -1,13 +1,13 @@
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 
-const OWNER_ID = "1535282595963600926";
+const ALLOWED_ROLE_ID = "1535282595963600926";
 const INVITE_URL = "https://discord.com/oauth2/authorize?client_id=1522251057302995004&permissions=2147699728&integration_type=0&scope=applications.commands+bot";
 
 module.exports = {
   name: "invite",
 
   async execute(message) {
-    if (message.author.id !== OWNER_ID) return;
+    if (!message.member?.roles.cache.has(ALLOWED_ROLE_ID)) return;
 
     const row = new ActionRowBuilder().addComponents(
       new ButtonBuilder()
