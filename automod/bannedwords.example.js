c0e44216e6a19d2e2ee8dffe471f18a763c278a8
simonpodhorny13-glitch <simonpodhorny13@gmail.com>
2026-08-07 20:20:19 +0200

    Add banned words template

---
+++ automod/bannedwords.example.js
@@ -0,0 +1,7 @@
+// Copy this file to bannedwords.js and replace the examples with your local filter list.
+// bannedwords.js is ignored by Git and will stay off the public repository.
+
+module.exports = [
+  "examplebadword",
+  "anotherexample"
+];
