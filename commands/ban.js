93dab6e8828caaea8956acc43968c4b1fd0e189a
simonpodhorny13-glitch <simonpodhorny13@gmail.com>
2026-08-07 20:23:22 +0200

    Log ban moderation cases

--- commands/ban.js
+++ commands/ban.js
@@ -1,4 +1,5 @@
 const { PermissionFlagsBits } = require("discord.js");
+const { recordModerationCase } = require("../moderation/logs");
 
 module.exports = {
   name: "ban",
@@ -23,6 +24,13 @@ module.exports = {
       return message.reply("❌ You can't ban yourself 😭");
     }
 
+    if (
+      message.guild.ownerId !== message.author.id &&
+      message.member.roles.highest.comparePositionTo(target.roles.highest) <= 0
+    ) {
+      return message.reply("❌ You can't ban a member with an equal or higher role.");
+    }
+
     if (!target.bannable) {
       return message.reply("❌ I can't ban that member. Check my role position and permissions.");
     }
@@ -35,15 +43,26 @@ module.exports = {
         `Reason: ${reason}\n` +
         `Moderator: ${message.author.tag}`
       );
-    } catch (error) {
+    } catch {
       console.log(`Could not DM banned user ${target.user.tag}.`);
     }
 
     await target.ban({ reason: `${reason} | Moderator: ${message.author.tag}` });
 
+    const caseId = await recordModerationCase({
+      guild: message.guild,
+      userId: target.id,
+      moderatorId: message.author.id,
+      action: "Ban",
+      detector: "Manual moderation",
+      reason,
+      channelId: message.channel.id,
+      messageId: message.id
+    });
+
     await message.channel.send(
       `🔨 **${target.user.tag}** was banned by **${message.author.tag}**.\n` +
-      `Reason: ${reason}`
+      `Reason: ${reason}\nCase: **#${caseId}**`
     );
   }
 };
