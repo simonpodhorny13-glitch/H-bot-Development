ac77d79dfc76a2cedb14e7c7ac8e5eb04abb73a6
simonpodhorny13-glitch <simonpodhorny13@gmail.com>
2026-08-07 15:43:21 +0200

    Add invite command module

---
+++ commands/invite.js
@@ -0,0 +1,10 @@
+// H bot Development - invite command
+// Command behavior will be added later.
+
+module.exports = {
+  name: "invite",
+
+  async execute(message, args) {
+    // TODO: Add invite command behavior.
+  }
+};
